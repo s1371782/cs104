a002c927c9ded04a5f7881e364dd45238f04ee33
s1371782 <s1371782@monmouth.edu>
2024-09-30 12:27:43 -0400

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/jvent/AppData/Local/Programs/Python/Python312/helloWorld.py
+Hello my name is Joseph
+>>> 
+= RESTART: C:/Users/jvent/AppData/Local/Programs/Python/Python312/helloWorld.py
+Hello my name is Joseph
